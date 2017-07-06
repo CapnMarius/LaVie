b255749279dddbe34390aa5ae4c337cbb0fa663b
MariusLinders <marius.linders@hotmail.com>
2017-07-06 16:33:18 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -10,43 +10,46 @@ mkdir -p ./backup/aurelia/aurelia_project
 mkdir -p ./backup/aurelia/crew
 mkdir -p ./backup/aurelia/web/cabin
 
-cp -r ./aurelia ./aurelia_backup_$current_time
-cp -r ./nodejs ./nodejs_backup_$current_time
+if cp -r ./aurelia ./aurelia_backup_$current_time && cp -r ./nodejs ./nodejs_backup_$current_time; then
+	cd /srv/http/database-editor/nodejs
+	mv ./environments ../backup/nodejs/environments
+	git reset --hard && git pull origin develop
+	rm -rf ./environments
+	cp -r ../backup/nodejs/* ./
 
-cd /srv/http/database-editor/nodejs
-mv ./environments ../backup/nodejs/environments
-git reset --hard && git pull origin develop
-rm -rf ./environments
-cp -r ../backup/nodejs/* ./
-
-if ./node_modules/.bin/grunt build; then
-	echo "NodeJS build successfull, restart database-editor service"
-else 
 	if ./node_modules/.bin/grunt build; then
 		echo "NodeJS build successfull, restart database-editor service"
 	else 
-		echo "NodeJS build failed. Please try a manual update"
+		if ./node_modules/.bin/grunt build; then
+			echo "NodeJS build successfull, restart database-editor service"
+		else 
+			echo "NodeJS build failed. Please try a manual update"
+		fi
 	fi
+
+	cd /srv/http/database-editor/aurelia
+	mv ./aurelia_project/environments ../backup/aurelia/aurelia_project/environments
+	mv ./Config ../backup/aurelia/Config
+	mv ./crew/zones.json ../backup/aurelia/crew/zones.json
+	mv ./yamaha_control.php ../backup/aurelia/yamaha_control.php
+	mv ./web/cabin/sources.json ../backup/aurelia/web/cabin/sources.json
+	git reset --hard && git pull origin develop
+	rm ./aurelia_project/environments
+	cp -r ../backup/aurelia/* ./
+
+	if ./node_modules/.bin/au build; then
+		echo "Aurelia build successfull"
+	else 
+	        echo "Aurelia build failed. Please try a manual update"
+	fi
+
+	echo "Don't forget to update kodi.service to restart always (
+		#sudo nano /lib/systemd/system/kodi.service 
+			on-abort -> always
+		#sudo systemctl daemon-reload && sudo systemctl restart kodi
+	)"
+	exit 1
+else
+	echo "Could not backup NodeJS and Aurelia. Aborted"
+	exit 1
 fi
-
-cd /srv/http/database-editor/aurelia
-mv ./aurelia_project/environments ../backup/aurelia/aurelia_project/environments
-mv ./Config ../backup/aurelia/Config
-mv ./crew/zones.json ../backup/aurelia/crew/zones.json
-mv ./yamaha_control.php ../backup/aurelia/yamaha_control.php
-mv ./web/cabin/sources.json ../backup/aurelia/web/cabin/sources.json
-git reset --hard && git pull origin develop
-rm ./aurelia_project/environments
-cp -r ../backup/aurelia/* ./
-
-if ./node_modules/.bin/au build; then
-        echo "Aurelia build successfull"
-else 
-        echo "Aurelia build failed. Please try a manual update"
-fi
-
-echo "Don't forget to update kodi.service to restart always (
-	#sudo nano /lib/systemd/system/kodi.service 
-		on-abort -> always
-	#sudo systemctl daemon-reload && sudo systemctl restart kodi
-)"
