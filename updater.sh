cd9c4696d8902b36c46773c78f560d245ce89fbf
MariusLinders <marius.linders@hotmail.com>
2017-07-06 16:45:14 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -13,18 +13,22 @@ mkdir -p ./backup/aurelia/web/cabin
 if cp -r ./aurelia ./aurelia_backup_$current_time && cp -r ./nodejs ./nodejs_backup_$current_time; then
 	cd /srv/http/database-editor/nodejs
 	mv ./environments ../backup/nodejs/environments
-	git reset --hard && git pull origin develop
-	rm -rf ./environments
-	cp -r ../backup/nodejs/* ./
+	
+	if git reset --hard && git pull origin develop; then
+		rm -rf ./environments
+		cp -r ../backup/nodejs/* ./
 
-	if ./node_modules/.bin/grunt build; then
-		echo "NodeJS build successfull, restart database-editor service"
-	else 
 		if ./node_modules/.bin/grunt build; then
-			echo "NodeJS build successfull, restart database-editor service"
+		echo "NodeJS build successfull, restart database-editor service"
 		else 
+			if ./node_modules/.bin/grunt build; then
+			echo "NodeJS build successfull, restart database-editor service"
+			else 
 			echo "NodeJS build failed. Please try a manual update"
+			fi
 		fi
+	else 
+		echo "NodeJS not updated"
 	fi
 
 	cd /srv/http/database-editor/aurelia
@@ -33,20 +37,24 @@ if cp -r ./aurelia ./aurelia_backup_$current_time && cp -r ./nodejs ./nodejs_bac
 	mv ./crew/zones.json ../backup/aurelia/crew/zones.json
 	mv ./yamaha_control.php ../backup/aurelia/yamaha_control.php
 	mv ./web/cabin/sources.json ../backup/aurelia/web/cabin/sources.json
-	git reset --hard && git pull origin develop
-	rm ./aurelia_project/environments
-	cp -r ../backup/aurelia/* ./
+	
+	if git reset --hard && git pull origin develop; then
+		rm ./aurelia_project/environments
+		cp -r ../backup/aurelia/* ./
 
-	if ./node_modules/.bin/au build; then
-		echo "Aurelia build successfull"
+		if ./node_modules/.bin/au build; then
+			echo "Aurelia build successfull"
+		else 
+		        echo "Aurelia build failed. Please try a manual update"
+		fi
 	else 
-	        echo "Aurelia build failed. Please try a manual update"
+		echo "Aurelia not updated"
 	fi
-
+	
 	echo "Don't forget to update kodi.service to restart always (
-		#sudo nano /lib/systemd/system/kodi.service 
+		sudo nano /lib/systemd/system/kodi.service 
 			on-abort -> always
-		#sudo systemctl daemon-reload && sudo systemctl restart kodi
+		sudo systemctl daemon-reload && sudo systemctl restart kodi
 	)"
 	exit 1
 else
